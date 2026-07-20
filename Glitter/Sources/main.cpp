83b885979509a6d075069cb2ec00f1c1b969c096
Noah Jutz <noah@noahjutz.com>
2026-07-20 16:43:53 +0200

    Add and load vertex shader

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -9,6 +9,7 @@
 // Standard Headers
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
@@ -42,6 +43,29 @@ int main(int argc, char *argv[]) {
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  // Load vertex shader
+
+  const char *vertexShaderSource =
+      "#version 330 core\n"
+      "layout (location = 0) in vec3 aPos;\n"
+      "void main()\n"
+      "{\n"
+      "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+      "}\0";
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  int success;
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+      char compileLog[512];
+      glGetShaderInfoLog(vertexShader, 512, NULL, compileLog);
+      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << std::endl << compileLog << std::endl;
+  }
+
   // Rendering Loop
   while (glfwWindowShouldClose(mWindow) == false) {
     if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -59,6 +83,6 @@ int main(int argc, char *argv[]) {
   return EXIT_SUCCESS;
 }
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  // glViewport(0, 0, width, height);
+void framebuffer_size_callback(GLFWwindow *, int width, int height) {
+  glViewport(0, 0, width, height);
 }
