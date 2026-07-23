8de92e68b4b68e8dd85241bf2abaa1f2d98e9649
Noah Jutz <noah@noahjutz.com>
2026-07-23 15:03:55 +0200

    Render a cube

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -51,12 +51,9 @@ int main() {
   glBindBuffer(GL_ARRAY_BUFFER, VBO1);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void *)0); // layout (loaction = 0) in vec3 aPos
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
                         (void *)(3 * sizeof(float)));
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void *)(6 * sizeof(float)));
   glEnableVertexAttribArray(0);
   glEnableVertexAttribArray(1);
   glEnableVertexAttribArray(2);
@@ -155,7 +152,7 @@ int main() {
     p1.setMat("view", V);
     p1.setMat("proj", P);
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 
     // Flip Buffers and Draw
     glfwSwapBuffers(mWindow);
